097f011fc8f323c8d4f2a404ec8282579cd5a93c
Alexander Llewellyn <alexander.llewellyn.pro@gmail.com>
2024-10-24 20:17:17 +0200

    debug

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,24 @@
 #
 #   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
 #   Character.create(name: "Luke", movie: movies.first)
+Flat.destroy_all
+
+Flat.create!(
+  name: 'Spacious Garden Flat London',
+  address: '10 Clifton Gardens London',
+  description: 'A lovely summer feel for this spacious garden flat. Two double bedrooms, open plan living area, large kitchen and a beautiful conservatory',
+  price_per_night: 75,
+  number_of_guests: 3
+)
+
+20.times do
+  Flat.create!(
+    name: Faker::Address.street_name,
+    address: Faker::Address.street_address,
+    description: Faker::Restaurant.review,
+    price_per_night: rand(50..300),
+    number_of_guests: rand(2..10)
+  )
+end
+
+puts "les apparts c'est good !"
